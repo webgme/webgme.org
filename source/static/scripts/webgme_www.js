6b84acf2ce41c9af001c5b98c9ddc94a55157a91
Laszlo Juracz <laszlo.juracz@vanderbilt.edu>
2014-04-02 00:20:14 -0500

    styling and thumbnails

---
+++ source/static/scripts/webgme_www.js
@@ -0,0 +1,13 @@
+/*Globals:$*/
+/**
+ * Created by laszlojuracz on 4/1/14.
+ */
+$(function() {
+    "use strict";
+
+    $('.fancybox').fancybox({
+        padding : 0,
+        openEffect  : 'elastic'
+    });
+
+});
