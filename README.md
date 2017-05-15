7d01ee4ee8d40b6ae6f3439f82989f754a9f1426
Hamid Zare <hamidzr@users.noreply.github.com>
2017-05-15 15:12:31 -0500

    Older browser support - transpiler for JavaScript. fixes #23 (#24)
    * transpiling and combining projects js files into
    one #23. adding webpack
    
    * removed duplicate css
    
    * cleaned up leftover webpack file
    
    * cleaned up leftover webpack file. fixed a clarity issue

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Setup the enviroment variables:
 - PORT
 - GOOGLE_ANALYTICS: to your google analytics key/id
 - NODE_ENV: production
+
+* npm run build to generate client-side js files
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const webpack = require('webpack'),
+    path = require('path');
+
+module.exports = {
+    cache: true,
+    devtool: "source-map",
+    entry: {
+        main: [
+            './src/client/sha512.js',
+            './src/client/helper.js',
+            './src/client/main.js'
+            ]
+        },
+    output: {
+        path: path.join(__dirname, '/public/') ,
+        filename: '[name].js',
+        chunkFilename: "[id].js",
+        sourceMapFilename: "[name].map"
+    },
+    module: {
+        rules: [
+            { test: /\.js$/, exclude: /node_modules/, use: [
+                { loader: "babel-loader",
+                    options: {
+                        minified: [true],
+                        compact: [true],
+                        sourceMaps: ['inline'],
+                        "presets": [
+                          "es2015"
+                        ]
+                    }
+                 }
+            ]},
+            // required to write "require('./style.scss')"
+            { test: /\.scss$/, use: [
+                { loader: "style-loader" },
+                { loader: "css-loader" },
+                { loader: "sass-loader" }
+            ]},
+            { test: /\.css$/,  use: [
+                { loader: "style-loader" },
+                { loader: "css-loader" }
+            ]}
+        ]
+    },
+    plugins: [
+        new webpack.LoaderOptionsPlugin({
+            debug: true
+        })
+        // new webpack.optimize.CommonsChunkPlugin({
+        //     name: "vendor",
+        //     filename: "vendor.js"
+        // }),
+    ]
+};
