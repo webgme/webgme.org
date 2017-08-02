9397dfb407cd16fd70e1142a8c55665d9c3e1f61
Patrik Meijer <patrik.meijer@vanderbilt.edu>
2017-08-02 13:43:55 -0500

    WIP Fix issues with admin creation.

--- editor/app.js
+++ editor/app.js
@@ -1,4 +1,4 @@
-// jshint node: true
+/*globals process*/
 'use strict';
 
 var gmeConfig = require('./config'),
@@ -9,30 +9,42 @@ webgme.addToRequireJsPaths(gmeConfig);
 
 myServer = new webgme.standaloneServer(gmeConfig);
 
-if (process.ENV.GME_ADMIN) {
-    var admin = process.ENV.GME_ADMIN.split(':');
+if (process.env.GME_ADMIN) {
+    var admin = process.env.GME_ADMIN.split(':'),
+        gmeAuth;
+
     if (admin.length !== 2) {
         throw new Error('GME_ADMIN should be of the format; user:password');
     }
 
     webgme.getGmeAuth(gmeConfig)
-        .then(function (gmeAuth) {
+        .then(function (gmeAuth_) {
+            gmeAuth = gmeAuth_;
             return gmeAuth.addUser(admin[0], 'em@il', admin[1], true, {siteAdmin: true});
         })
-        .finally(function (err) {
-            if (err) {
-                if (err.message.indexOf('user already exists') > -1) {
-                    console.log('Provided admin user already existed');
-                } else {
-                    console.error('Failed to create admin', process.ENV.GME_ADMIN, err);
-                    process.exit(1);
-                }
+        .then(function () {
+            console.log('Added new siteAdmin user:', admin[0]);
+        })
+        .catch(function (err) {
+            if (err.message.indexOf('user already exists') > -1) {
+                console.log('Admin user already existed: ', admin[0]);
+            } else {
+                console.error('Failed to create admin', process.env.GME_ADMIN, err);
+                process.exit(1);
             }
+        })
+        .finally(function () {
+            gmeAuth.unload()
+                .then(function () {
+                    myServer.start();
+                })
+                .catch(function (err) {
+                    console.err(err);
+                    process.exit(1);
+                });
+        })
+        .done();
 
-            myServer.start(function () {
-                //console.log('server up');
-            });
-        });
 } else {
     myServer.start(function () {
         //console.log('server up');
