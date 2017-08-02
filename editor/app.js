333c798771b89985b17c4c830e9adad8b151cb12
Patrik Meijer <patrik.meijer@vanderbilt.edu>
2017-08-02 10:55:26 -0500

    WIP Check GME_ADMIN and add specified admin at start up

--- editor/app.js
+++ editor/app.js
@@ -8,6 +8,34 @@ var gmeConfig = require('./config'),
 webgme.addToRequireJsPaths(gmeConfig);
 
 myServer = new webgme.standaloneServer(gmeConfig);
-myServer.start(function () {
-    //console.log('server up');
-});
+
+if (process.ENV.GME_ADMIN) {
+    var admin = process.ENV.GME_ADMIN.split(':');
+    if (admin.length !== 2) {
+        throw new Error('GME_ADMIN should be of the format; user:password');
+    }
+
+    webgme.getGmeAuth(gmeConfig)
+        .then(function (gmeAuth) {
+            return gmeAuth.addUser(admin[0], 'em@il', admin[1], true, {siteAdmin: true});
+        })
+        .finally(function (err) {
+            if (err) {
+                if (err.message.indexOf('user already exists') > -1) {
+                    console.log('Provided admin user already existed');
+                } else {
+                    console.error('Failed to create admin', process.ENV.GME_ADMIN, err);
+                    process.exit(1);
+                }
+            }
+
+            myServer.start(function () {
+                //console.log('server up');
+            });
+        });
+} else {
+    myServer.start(function () {
+        //console.log('server up');
+    });
+}
+
