e31eea3b861263f32a20c6c08d30b605b9219b97
Patrik Meijer <patrik.meijer@vanderbilt.edu>
2017-08-02 12:03:07 -0500

    WIP Fix error handling in config.docker.js

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
-# Deployment configuration and static webgme page
+See /aws/README.md for more aws specific directions.
 
-See /aws/README.md for more directions.
+### To launch the editor from docker
+
+Create an empty directory to share with the containers (e.g. ~/dockershare)
+```mkdir ~/dockershare```
+
+Launch a mongo container (it must be named mongo)
+```docker run -d -v ~/dockershare/db:/data/db --name mongo mongo```
+
+Launch a webgme-org container
+```docker run -d -p 8888:8001 -v ~/dockershare:/dockershare --link mongo:mongo --name=webgme -e "GME_ADMIN=admin:admin" webgme/webgme-org```
+
+Webgme should be available at `localhost:8888`
+
+#### Notes
+The env variable `GME_ADMIN` will create a siteAdmin `admin` with password `admin` if it does not exist. 
+After the first launch - go to `localhost:8888/profile/login` and change password once logged in.
+
+To overwrite configuration parameters, stop the webgme container and create a file `~/dockershare/extraconfigs.js`, see aws/extraconfigs.js for the structure.
+After restart the config parameters will be picked up by the webgme app.
+
+Typically webgme should run behind a secure proxy, see aws/nginx.conf for an example of a config for nginx.
--- editor/config/config.docker.js
+++ editor/config/config.docker.js
@@ -1,6 +1,7 @@
+/*globals process, console, require*/
 'use strict';
-var path = require('path'),
-    fs = require('fs'),
+
+var fs = require('fs'),
     config = require('./config.default'),
     validateConfig = require('webgme/config/validator');
 
@@ -13,19 +14,31 @@ config.authentication.jwt.privateKey = '/dockershare/token_keys/private_key';
 config.authentication.jwt.publicKey = '/dockershare/token_keys/public_key';
 
 // Make sure there are some keys for token generation
-if (fs.lstatSync('/dockershare/token_keys').isDirectory() === false) {
-    fs.mkdirSync('/dockershare/token_keys');
+try {
+    if (fs.lstatSync('/dockershare/token_keys').isDirectory() === false) {
+        console.error('/dockershare/token_keys exists but is not a directory');
+        process.exit(1);
+    }
+} catch (e) {
+    if (e.code = 'ENOENT') {
+        fs.mkdirSync('/dockershare/token_keys');
+    }
 }
 
-if ((fs.lstatSync('/dockershare/token_keys/private_key').isFile() &&
-    fs.lstatSync('/dockershare/token_keys/public_key').isFile()) === false) {
-
-    console.error('Token keys did not exist - will generate them (key size 1024)');
-    var NodeRSA = require('node-rsa'),
-        key = new NodeRSA({b: 1024});
-
-    fs.writeFileSync('/dockershare/token_keys/private_key', key.exportKey('pkcs1-private'));
-    fs.writeFileSync('/dockershare/token_keys/public_key', key.exportKey('pkcs8-public'));
+try {
+    if ((fs.lstatSync(config.authentication.jwt.privateKey).isFile() &&
+        fs.lstatSync(config.authentication.jwt.publicKey).isFile()) === false) {
+        console.error('token keys exists but both are not files (?)');
+        process.exit(1);
+    }
+} catch (e) {
+    if (e.code = 'ENOENT') {
+        console.error('Token keys did not exist - will generate them (key size 1024)');
+        var NodeRSA = require('node-rsa'),
+            key = new NodeRSA({b: 1024});
+        fs.writeFileSync(config.authentication.jwt.privateKey, key.exportKey('pkcs1-private'));
+        fs.writeFileSync(config.authentication.jwt.publicKey, key.exportKey('pkcs8-public'));
+    }
 }
 
 config.blob.fsDir = '/dockershare/blob-local-storage';
@@ -40,8 +53,11 @@ config.mongo.uri = 'mongodb://' + process.env.MONGO_PORT_27017_TCP_ADDR + ':' + 
 try {
     require('/dockershare/extraconfigs.js')(config);
 } catch (e) {
-    logger.error(e);
-
+    if (e.message.indexOf('Cannot find module') > -1) {
+        console.log('/dockershare/extraconfigs.js not provided - will go on with defaults');
+    } else {
+        console.error('Problems with /dockershare/extraconfigs.js', e);
+    }
 }
 
 validateConfig(config);
