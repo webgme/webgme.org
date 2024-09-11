690605562426bee6d35f1dd0997b61c1d162e11d
Patrik Meijer <patrik.meijer@vanderbilt.edu>
2024-09-11 15:48:51 -0400

    Add back stopping of nginx during certs update

--- aws/renew_certs.sh
+++ aws/renew_certs.sh
@@ -7,11 +7,13 @@ docker compose -f $COMPOSE_YML stop web
 if ! certbot renew > /var/log/letsencrypt/renew.log 2>&1 ; then
     echo Automated renewal failed:
     cat /var/log/letsencrypt/renew.log
+    systemctl stop nginx
     tail /var/log/letsencrypt/letsencrypt.log
     docker compose -f $COMPOSE_YML up --no-recreate -d web
     exit 1
 fi
 
+systemctl stop nginx
 tail /var/log/letsencrypt/letsencrypt.log
 # Copy over the new certs
 cp /etc/letsencrypt/live/${HOSTNAME}/privkey.pem /home/ubuntu/dockershare/ssl_certs/privkey.pem
